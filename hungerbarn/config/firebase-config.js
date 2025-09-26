773dce480ed6f538e1af7551cf8c5c6178af4ec9
Krutiventi vansh <82541891+kvansh7@users.noreply.github.com>
2025-09-26 12:12:33 +0530

    Update firebase-config.js

--- hungerbarn/config/firebase-config.js
+++ hungerbarn/config/firebase-config.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics, isSupported } from "firebase/analytics"; // import Analytics
+
 const firebaseConfig = {
   apiKey: "AIzaSyBnltMmwqDXVWbdjpdDtc8xzKQS20Q_tYc",
   authDomain: "hungerbarn-a3647.firebaseapp.com",
@@ -8,11 +10,22 @@ const firebaseConfig = {
   storageBucket: "hungerbarn-a3647.appspot.com",
   messagingSenderId: "1034294918772",
   appId: "1:1034294918772:web:ca0fe468e73354c5e8db21",
-  measurementId: "G-G4HT79E790"
+  measurementId: "G-G4HT79E790" // Make sure this is correct
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firestore = getFirestore(firebaseApp);
 const storage = getStorage(firebaseApp);
 
-export { firebaseApp, firestore, storage };
+// Initialize Analytics safely
+let analytics = null;
+isSupported().then((supported) => {
+  if (supported) {
+    analytics = getAnalytics(firebaseApp);
+    console.log("Google Analytics initialized");
+  } else {
+    console.log("Google Analytics not supported in this environment.");
+  }
+});
+
+export { firebaseApp, firestore, storage, analytics };
